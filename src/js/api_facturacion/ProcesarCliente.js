bc61534e0af75702a0a62cad730dbdb6842f1cf5
jessicalyruiz <jessicalyr@gmail.com>
2023-02-22 19:46:12 -0500

    taller33 vue

---
+++ src/js/api_facturacion/ProcesarCliente.js
@@ -0,0 +1,20 @@
+
+import axios from "axios";
+//metodos fachada
+
+
+
+
+//metodos de consumo
+//crud
+
+const obtenerClientePorCedula=(cedula)=>{
+
+}
+const insertar=(body)=>{
+
+}
+
+const actualizar=(body)=>{
+
+}
