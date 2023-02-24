5ede6bee4fa764e342f96364640bdde211bf67de
jessicalyruiz <jessicalyr@gmail.com>
2023-02-23 19:46:35 -0500

    taller 34

--- src/js/api_facturacion/ProcesarCliente.js
+++ src/js/api_facturacion/ProcesarCliente.js
@@ -1,20 +1,50 @@
 
 import axios from "axios";
 //metodos fachada
+export const obtenerClientePorCedulaFachada=async(cedula)=>{
+    return  await obtenerClientePorCedula(cedula);
+}
+export const insertarFachada=async(body)=>{
+ insertar(body)
+}
+export const actualizarFachada=(id,body)=>{
 
+}
+export const buscarTodosFachada=(id)=>{
 
+}
 
+export const borrarPorIdFachada=(id)=>{
+
+}
+export const borrarPorCedulaFachada=(cedula)=>{
+
+}
 
 //metodos de consumo
 //crud
 
-const obtenerClientePorCedula=(cedula)=>{
+const obtenerClientePorCedula=async(cedula)=>{
+   const datos=axios.get(`http://localhost:8081/API/Facturacion/V1/clientes/${cedula}`).then(r=>r.data)
+   console.log(datos)
+   //console.log("apellido"+apellido)
+   //console.log(cedula)
+   return datos
+}
+const insertar=async(body)=>{
+    axios.post(`http://localhost:8081/API/Facturacion/V1/clientes`,body).then(r=>r.data)
 
 }
-const insertar=(body)=>{
+
+const actualizar=(id,body)=>{
+    axios.put(`http://localhost:8081/API/Facturacion/V1/clientes`,body).then(r=>r.data)
 
 }
 
-const actualizar=(body)=>{
+const borrarPorId=(id)=>{
+    axios.delete(`http://localhost:8081/API/Facturacion/V1/clientes/${id}`,body).then(r=>r.data)
+
+}
+const borrarPorCedula=()=>{
 
 }
---
+++ src/pages/BuscarPorCedula.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>Buscar por cedula</h1>
+  <input v-model="cedula" type="text">
+    <button v-on:click="consultarCliente">Consultar</button>
+    <button v-on:click="insertar">Insertar</button>
+</template>
+
+<script>
+import {obtenerClientePorCedulaFachada,insertarFachada} from "../js/api_facturacion/ProcesarCliente.js"
+
+
+export default {
+    data(){
+        return{
+        cedula:null
+    }
+    },
+    methods:{
+        consultarCliente(){
+           const cliente= obtenerClientePorCedulaFachada(this.cedula)
+           console.log(cliente.nombre)
+        },
+        insertar(){
+            const miCliente={
+                apellido:"ysdanez",
+                nombre: "jessidsca",
+                cedula:"1700",
+                fechaNacimiento:"1992-06-24",
+                genero:"F",
+                email:"jasd@gmailcom"
+            
+        }
+        insertarFachada(miCliente);
+    }}
+}
+</script>
+
+<style>
+
+</style>
